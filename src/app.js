737befdb4a9a5b89345c05225ba98120aae39b4b
Denys Mazur <dmazur.ua@gmail.com>
2022-01-26 12:22:57 +0200

    add static folder

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const userRouter = require('./user/UserRouter')
 const statisticRouter = require('./usersStatistic/UsersStatisticRouter')
@@ -13,5 +14,14 @@ app.use(cors())
 app.use('/api/users', userRouter)
 app.use('/api/users-statistic', statisticRouter)
 app.use(ErrorHandler)
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 module.exports = app
